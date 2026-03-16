fd34c3abfda4142664089254395f7171db8c2550
wiki-keshav <wiki.work08@gmail.com>
2026-03-16 14:03:52 +0530

    F-strings are a way to format strings in Python.

---
+++ f-string.py
@@ -0,0 +1,15 @@
+'''if we add f infront of a string and put variables and operations in placeholders{} 
+then a variable can contain both string and numbers '''
+
+
+price=6
+txt=f"The price of banana is {price} rupees"
+print(txt)
+
+
+#we can also perform operations in the placeholders
+
+
+quantity=12
+txt2=f"The price of {quantity} bananas is {quantity * price} rupees"
+print(txt2)
