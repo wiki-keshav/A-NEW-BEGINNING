e644b2720cd549c848f059c0b09770bf58f045a0
wiki-keshav <wiki.work08@gmail.com>
2026-03-16 14:15:01 +0530

    modifiers in f-string

--- f-string.py
+++ f-string.py
@@ -2,14 +2,17 @@
 then a variable can contain both string and numbers '''
 
 
-price=6
+price=6.35
 txt=f"The price of banana is {price} rupees"
 print(txt)
 
 
 #we can also perform operations in the placeholders
+#A placeholder can include a modifier to format the value.
 
 
 quantity=12
-txt2=f"The price of {quantity} bananas is {quantity * price} rupees"
+txt2=f"The price of {quantity} bananas is {quantity * price:.2f} rupees"
 print(txt2)
+
+
